a604848d63da5409b730dc219d8cfa326ba48474
ruomu29 <ruomuwang29@gmail.com>
2025-03-28 01:33:28 +0000

    fixing bugs of chat list page last message time shown

--- services/chatService.js
+++ services/chatService.js
@@ -1,6 +1,6 @@
 // services/chatService.js
 // most are copied from original components/allChats app/chatMessages
-import { collection, getDocs, onSnapshot, query, orderBy, addDoc, serverTimestamp } from 'firebase/firestore';
+import { collection, getDocs, onSnapshot, query, orderBy, addDoc, serverTimestamp, doc, updateDoc } from 'firebase/firestore';
 import { db } from '@/config/firebaseConfig';
 
 // load all chats from database
@@ -38,10 +38,20 @@ export function subscribeToMessages(chatId, callback, errorCallback) {
 
 // sending message
 export async function sendMessage(chatId, newMessage, user) {
+  const trimmed = newMessage.trim();
+  const now = serverTimestamp();
+
   const messagesRef = collection(db, 'chats', chatId.toString(), 'messages');
+
   await addDoc(messagesRef, {
-    value: newMessage.trim(),
-    created_at: serverTimestamp(),
-    user_id : user
+    value: trimmed,
+    created_at: now,
+    user_id: user,
+  });
+
+  const chatDocRef = doc(db, 'chats', chatId.toString());
+  await updateDoc(chatDocRef, {
+    lastMessage: trimmed,
+    lastMessageTime: now,
   });
 }
